9fa398ee35d870c96bad2c5a6a06b1f6a94b4080
zhangsh <zhangshuhua411@hotmail.com>
2016-03-28 20:35:50 +0800

    优化

--- admin/src/main/java/com/warehouse/web/action/LoginAction.java
+++ admin/src/main/java/com/warehouse/web/action/LoginAction.java
@@ -1,7 +1,5 @@
 package com.warehouse.web.action;
 
-import javax.annotation.Resource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
