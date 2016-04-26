9fac1da8bffb6358d4695cd7f83466d9941f52e1
zhangsh <zhangshuhua411@hotmail.com>
2016-04-26 13:07:30 +0800

    增加一个测试，但是不能用

---
+++ admin/src/test/java/admin/LoginActionTest.java
@@ -0,0 +1,23 @@
+package admin;
+
+import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.warehouse.web.action.LoginAction;
+
+/**
+ * 好多省略的东西
+ * @author zhangsh
+ *
+ */
+public class LoginActionTest {
+	@Test
+	public void testLogin(){
+		/*LoginAction la = new LoginAction();
+		MockMvc mockMvc = MockMvcBuilders.standaloneSetup(la).build();
+		RequestBuilder requestBuilder = RequestBuilder.class.newInstance();
+		mockMvc.perform(requestBuilder).andExpect(matcher)*/
+	}
+}
